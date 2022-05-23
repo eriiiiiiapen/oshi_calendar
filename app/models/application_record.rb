940f191253137028028663494bdcf269a73831df
eriiiiiiapen <7muraeri@gmail.com>
2022-05-24 01:21:25 +0900

    Add:ログイン、ログアウト機能の実装（Sorceryを用いて）

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-<<<<<<< HEAD
   self.abstract_class = true
-=======
-  primary_abstract_class
->>>>>>> main
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
   get 'privacy', to: 'static_pages#privacy'
+  resources :users, only: %i[new create edit update]
 end
